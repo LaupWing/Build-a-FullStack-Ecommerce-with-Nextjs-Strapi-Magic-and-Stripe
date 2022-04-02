e1c7a756ba60cd4cbb9820083182b3573b54fdc1
LaupWing <loc_strict@hotmail.com>
2022-04-02 19:52:47 +0200

    update

--- ecommerce-frontend/pages/products/[slug].js
+++ ecommerce-frontend/pages/products/[slug].js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import products from '../../data/products.json'
 import { fromImageToUrl } from '../../utils/urls'
 const product = products.data[0]
@@ -5,6 +6,14 @@ const product = products.data[0]
 const Product = () =>{
    return (
       <div>
+         <Head>
+            {product.attributes.meta_title && 
+               <title>{product.attributes.meta_title}</title>
+            }
+            {product.attributes.meta_description && 
+               <meta name='description' content={product.attributes.meta_description}/>
+            }
+         </Head>
          <h3>{product.attributes.name}</h3>
          <img 
             src={fromImageToUrl(product.attributes.image.data.attributes)}
