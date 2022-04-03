c62c9bcbafd1a8802b0b60bec456e5bedc0dd534
LaupWing <loc_strict@hotmail.com>
2022-04-03 18:44:23 +0200

    context is working

---
+++ ecommerce-frontend/pages/account.js
@@ -0,0 +1,32 @@
+import React, { useContext } from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+import AuthContext from '../context/AuthContext'
+
+const Account = () => {
+   const {logoutUser} = useContext(AuthContext)
+
+   if(!user){
+      return (
+         <div>
+            <p>Please login or register</p>
+            <Link href={'/'}>
+               <a>Go back</a>
+            </Link>
+         </div>)
+   }
+
+   return (
+      <div>
+         <Head>
+            <title>Account Page</title>
+            <meta name='description' content='The account page, view your orders' />
+         </Head>
+         <h2>Account page</h2>
+
+         <a href='#' onClick={logoutUser}>Logout</a>
+      </div>
+   )
+}
+
+export default Account
