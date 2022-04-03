0d1c200c1f727b48b5adc81c59757a549b76d820
LaupWing <loc_strict@hotmail.com>
2022-04-03 19:05:59 +0200

    update

--- ecommerce-frontend/pages/login.js
+++ ecommerce-frontend/pages/login.js
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react'
 import Head from 'next/head'
 import AuthContext from '../context/AuthContext'
+import styles from '../styles/Login.module.css'
 
 const Login = () => {
    const {loginUser} = useContext(AuthContext)
@@ -21,12 +22,16 @@ const Login = () => {
          <h2>Login</h2>
          <form onSubmit={handleSubmit}>
             <input 
+               className={styles.input}
                type="email"
                value={email}
                onChange={event => setEmail(event.target.value)}
                placeholder='Email Address'
             />
-            <button type="submit">Login</button>
+            <button 
+               type="submit"
+               className={styles.button}
+            >Login</button>
          </form>
       </div>
    )
